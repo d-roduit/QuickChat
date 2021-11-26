c04c64202e3b9a6ec6350305bf3de304a3d1766a
d-roduit <37080343+d-roduit@users.noreply.github.com>
2021-11-26 17:14:38 +0100

    Add helper methods for handling cookies

---
+++ src/main/java/ch/droduit/quickchat/helper/CookieHelper.java
@@ -0,0 +1,25 @@
+package ch.droduit.quickchat.helper;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public class CookieHelper {
+
+    public static String extractCookie(HttpServletRequest request, String cookieName) {
+        Cookie[] cookies = request.getCookies();
+        if (cookies == null) return null;
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals(cookieName))
+                return cookie.getValue();
+        }
+        return null;
+    }
+
+    public static Cookie createCookie(String UUID, String value, String path) {
+        Cookie cookie = new Cookie(UUID, value);
+        cookie.setMaxAge(60 * 60 * 24 * 365);
+        cookie.setPath(path);
+        return cookie;
+    }
+
+}
