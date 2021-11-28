5bd6b3d42a30add96be483db8836a84d61bf7865
d-roduit <37080343+d-roduit@users.noreply.github.com>
2021-11-28 10:12:56 +0100

    Remove unused JavaScript CookieHelper

--- src/main/resources/static/js/helper/CookieHelper.js
+++
@@ -1,6 +0,0 @@
-const CookieHelper = {
-    createCookie: (name, value, path) => {
-        const maxAge = 60 * 60 * 24 * 365;
-        document.cookie = `${name} = ${value}; max-age = ${maxAge}; path = ${path}`;
-    }
-};
